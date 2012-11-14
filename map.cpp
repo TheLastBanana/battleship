3d17c9af9e30f7c47c4dc0514b87be671af4f9ca
Braedy Kuzma <puckmaster_84@hotmail.com>
2012-11-14 13:14:52 -0700

    InitMap now works as intended.

--- map.cpp
+++ map.cpp
@@ -1,45 +1,54 @@
+#include "Arduino.h"
 #include <map.h>
 #include <ship.h>
 
-map initMap(Map* map, uint8_t initialState) {
-  map->ships[0] = {
-    -1,
-    -1, 
-    PATROL_HEALTH,
-    0,
-    TYPES.PATROL
-  };
-  map->ships[0] = {
-    -1,
-    -1, 
-    SUBMARINE_HEALTH,
-    0,
-    TYPES.SUBMARINE
-  };
-  map->ships[0] = {
-    -1,
-    -1, 
-    DESTROYER_HEALTH,
-    0,
-    TYPES.DESTROYER
-  };
-  map->ships[0] = {
-    -1,
-    -1, 
-    BATTLESHIP_HEALTH,
-    0,
-    TYPES.BATTLESHIP
-  };
-  map->ships[0] = {
-    -1,
-    -1, 
-    CARRIER_HEALTH,
-    0,
-    TYPES.CARRIER
-  };
+/**
+ * Initializes a map struct with default values.
+ * Fills ships in order Patrol>Sub>Destroyer>Battleship>Carrier
+ * with coordinates (-1, -1), the default ship health for each,
+ * facing right, and each ship type.
+ * @param map Pointer to the map to fill.
+ * @param initialState The state to initialize each square to.
+ */
+Map initMap(Map* curMap, uint8_t initialState) {
+  //Patrol
+  curMap->ships[0].x = -1;
+  curMap->ships[0].y = -1;
+  curMap->ships[0].health = PATROL_HEALTH;
+  curMap->ships[0].direction = Ship::RIGHT;
+  curMap->ships[0].type = Ship::PATROL;
+
+  //Submarine
+  curMap->ships[1].x = -1;
+  curMap->ships[1].y = -1;
+  curMap->ships[1].health = SUBMARINE_HEALTH;
+  curMap->ships[1].direction = Ship::RIGHT;
+  curMap->ships[1].type = Ship::SUBMARINE;
+
+  //Destroyer
+  curMap->ships[2].x = -1;
+  curMap->ships[2].y = -1;
+  curMap->ships[2].health = DESTROYER_HEALTH;
+  curMap->ships[2].direction = Ship::RIGHT;
+  curMap->ships[2].type = Ship::DESTROYER;
+
+  //Battleship
+  curMap->ships[3].x = -1;
+  curMap->ships[3].y = -1;
+  curMap->ships[3].health = BATTLESHIP_HEALTH;
+  curMap->ships[3].direction = Ship::RIGHT;
+  curMap->ships[3].type = Ship::BATTLESHIP;
+
+  //Carrier
+  curMap->ships[4].x = -1;
+  curMap->ships[4].y = -1;
+  curMap->ships[4].health = CARRIER_HEALTH;
+  curMap->ships[4].direction = Ship::RIGHT;
+  curMap->ships[4].type = Ship::CARRIER;
+
   
   for(uint8_t i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
-    map->squares[i] = initialState;
+    curMap->squares[i] = initialState;
   }
 }
 
