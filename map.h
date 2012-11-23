c5760735ec5f5663ee7ae639bf762541062a553f
kuzi117 <puckmaster_84@hotmail.com>
2012-11-23 12:33:29 -0700

    Added getShipIndex()

--- map.h
+++ map.h
@@ -27,5 +27,6 @@ void setShipType(uint8_t* block, Ship::TYPES type);
 uint8_t indexFromPos(uint8_t x, uint8_t y);
 void posFromIndex(uint8_t index, uint8_t *x, uint8_t *y);
 bool shipClear(Map* map, Ship* ship);
+uint8_t getShipIndex(Ship::TYPES type);
 
 #endif
